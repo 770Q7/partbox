36bb6d33da8da683c26e029cab007863b2dda899
770Q7 <141187425+770Q7@users.noreply.github.com>
2025-06-02 19:28:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,2 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCiU2CqXcpWa7gLeJz0_-5dqzmgCig3Be8",
-    authDomain: "accounts-e6751.firebaseapp.com",
-    projectId: "accounts-e6751",
-    storageBucket: "accounts-e6751.firebasestorage.app",
-    messagingSenderId: "813001288493",
-    appId: "1:813001288493:web:6a296b6f1b2ca35f1b41a4"
-};
+fixed
+##############################################################################################################################------------------------------------------------------------------------------------------------
